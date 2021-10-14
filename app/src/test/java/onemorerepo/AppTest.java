ee5ca663724d60c867c6102b3c13daada4924905
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-10-14 18:02:43 +0200

    added new test

--- app/src/test/java/onemorerepo/AppTest.java
+++ app/src/test/java/onemorerepo/AppTest.java
@@ -11,4 +11,8 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+    @Test void appHasTheRightGreeting() {
+        App classUnderTest = new App();
+        assertEquals(classUnderTest.getGreeting(), "Hello World!");
+    }
 }
